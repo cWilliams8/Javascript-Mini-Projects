836c9318fbb52e4618e2edfa693cd389f4af6797
cWilliams8 <c_williams08@yahoo.com>
2024-04-08 23:45:19 -0400

    Added Color Picker and Palindrome Checker

---
+++ mini js pros/palindrome/index.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("input")
+
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+
+function check() {
+    const value = input.value;
+    const reverse = reverseString(value);
+    if(value === reverse) {
+        alert("This is a palindrome!")
+    } else {
+        alert("This is NOT a palindrome :(")
+    };
+
+    input.value = "";
+}
